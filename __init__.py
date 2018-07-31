c2ea72f91cad8920d9ccc332b570e259d35b226d
CAronoff <caronoff@gmail.com>
2018-07-31 11:54:34 -0400

    file upload

--- __init__.py
+++ __init__.py
@@ -304,6 +304,10 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
 @app.route('/batch', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -320,8 +324,12 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+
+            text = open(filename, 'r+')
+            content = text.read()
+            text.close()
+            return render_template('content.html', text=content)
+
     return redirect(url_for('decode'))
 
 if __name__ == "__main__":
